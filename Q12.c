d2447943725ea4771b3a88d69187dea5d0f0dbef
juanphf <jphferreira@outlook.com>
2024-03-25 23:08:54 -0300

    Questoes

--- Q12.c
+++ Q12.c
@@ -1,8 +1,8 @@
-// aloha[2] = value;
-// scanf("%f", &aloha);
-// aloha = "value";
-// printf("%f", aloha);
-// coisas[4][4] = aloha[3];
-// coisas[5] = aloha;
-// pf = value;
-// pf = aloha; // Valido, ponteiro apontando para um array
+// aloha[2] = value;  // Valido, o array aloha de indice 2 recebe o valor da variavel value.
+// scanf("%f", &aloha);  // Invalido, é necessário especificar um índice. 
+// aloha = "value";  // Invalido, pois esta atribuindo uma string diretamente a um array de float.
+// printf("%f", aloha); // Invalido, imprimir esse ponteiro diretamente usando o especificador de formato %f é inválido porque você está tentando imprimir um ponteiro como um float.
+// coisas[4][4] = aloha[3]; // Valido, a matriz na coluna 4 linha 4 recebe o valor que está no vetor no índice 3.
+// coisas[5] = aloha; // Invalido, coisas é matriz e não array, e aloha não está especificado o índice.
+// pf = value; // Invalido, ponteiro não pode direcionar direto para uma variavel e não para seu endereço de memória.
+// pf = aloha; // Valido, ponteiro apontando para um array.
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Juan Pablo
+Juan Pablo H. Ferreira
+20190073695
